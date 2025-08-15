23c48dc2c9495e1d63658819df2088d5f2391bb0
Rishit <patelrishit.4311@gmail.com>
2025-08-15 13:36:31 +0530

    01 & 02 - Setup, Database

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+// import {Button} from '@/components/ui/button'
+
+export default function HomePage() {
+  return (
+  <div className="text-4xl font-bold text-green-500"><h1>MEET AI</h1></div>
+  )
+}
