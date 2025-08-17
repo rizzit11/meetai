118273aceef95a5609a2bf73a07ea97e74ba67a2
Rishit <patelrishit.4311@gmail.com>
2025-08-17 22:05:52 +0530

    06-dashboard-sidebar

---
+++ src/modules/dashboard/ui/componenets/dashboard-sidebar.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import { BotIcon, StarIcon, VideoIcon } from "lucide-react"
+import Link from 'next/link'
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+import { Separator } from "@/components/ui/separator"
+import { usePathname } from 'next/navigation'
+import { DashboardUserButton } from "./dashboard-user-button"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+
+const firstSection = [
+    {
+        icon : VideoIcon,
+        label : "Meetings",
+        href : "/meetings",
+    },
+    {
+        icon: BotIcon,
+        label : "Agents",
+        href : "/agents",
+    }
+]
+const secondSection = [
+    {
+        icon : StarIcon,
+        label : "Upgrade",
+        href : "/upgrade",
+    }
+]
+
+export const DashboardSidebar = () => {
+    const pathname = usePathname()
+    return(
+        <Sidebar>
+            <SidebarHeader className="text-sidebar-accent-foreground">
+                <Link href="/" className="flex-items-center gap-2 px-2 pt-2">
+                <Image src="/logo.svg" width={34} height={34} alt="Meet AI" />
+                <p className="text-2xl font-semibold">Meet.AI</p>
+                </Link>
+            </SidebarHeader>
+            <div className="px-2 py-2">
+                <Separator className="opacity-10 text-[#5D6B68]" />
+            </div>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {firstSection.map((item) => (
+                                <SidebarMenuItem key={item.href}>
+                                    <SidebarMenuButton asChild className={cn(
+                                        "h-10 hover:bg-linear-to-r/oklch border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-sidebar/50 to sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                                    )}
+                                    isActive={pathname === item.href}
+                                    >
+                                        <Link href={item.href}>
+                                          <item.icon className="size-5" />
+                                          <span className="text-sm font-medium tracking-tight">{item.label}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                <div className="px-2 py-2">
+                    <Separator className="opacity-10 text-[#5D6B68]" />
+                </div>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {secondSection.map((item) => (
+                                <SidebarMenuItem key={item.href}>
+                                    <SidebarMenuButton asChild className={cn(
+                                        "h-10 hover:bg-linear-to-r/oklch border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-sidebar/50 to sidebar/50",
+                                        pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                                    )}
+                                    isActive={pathname === item.href}
+                                    >
+                                        <Link href={item.href}>
+                                          <item.icon className="size-5" />
+                                          <span className="text-sm font-medium tracking-tight">{item.label}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter className="text-white">
+                <DashboardUserButton />
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
