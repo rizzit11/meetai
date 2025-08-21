c6431922b019398df5de890a92871e754a854b6a
Rishit <patelrishit.4311@gmail.com>
2025-08-21 22:37:37 +0530

    11: agents form

---
+++ src/modules/agents/schemas.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod'
+ 
+export const agentsInsertSchema = z.object({
+    name: z.string().min(1, {message: "Name is Required"}),
+    instructions: z.string().min(1, {message: "Intstructions are Required"}),
+})
