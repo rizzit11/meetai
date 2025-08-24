4317995c2bd9bd670e240d1d26abe1b20f347b08
Rishit <patelrishit.4311@gmail.com>
2025-08-24 19:56:04 +0530

    19: Meeting Filters

--- src/components/command-select.tsx
+++ src/components/command-select.tsx
@@ -37,6 +37,11 @@ export const CommandSelect = ({
   const [open, setOpen] = useState(false);
   const selectedOption = options.find((option) => option.value === value);
 
+  const handleOpenChange = (value: boolean) => {
+    onSearch?.("")
+    setOpen(value)
+  }
+
   return (
     <>
       <Button
@@ -55,7 +60,7 @@ export const CommandSelect = ({
       <CommandResponsiveDialog
         shouldFilter={!onSearch}
         open={open}
-        onOpenChange={setOpen}
+        onOpenChange={handleOpenChange}
       >
         <CommandInput placeholder="Search..." onValueChange={onSearch} />
         <CommandList>
--- src/modules/meetings/server/procedures.tsx
+++ src/modules/meetings/server/procedures.tsx
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import { agents ,meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
@@ -11,31 +11,32 @@ import {
 } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
+import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
-   // Procedure for updating Meetings
-    // Updates a Meeting by ID
-    // Returns the updated Meeting
-    update: protectedProcedure
-      .input(meetingsUpdateSchema)
-      .mutation(async ({ input, ctx }) => {
-        const [updatedMeeting] = await db
-          .update(meetings)
-          .set(input)
-          .where(
-            and(
-              eq(meetings.id, input.id),
-              eq(meetings.userId, ctx.auth.user.id)
-            )
-          )
-          .returning();
+  // Procedure for updating Meetings
+  // Updates a Meeting by ID
+  // Returns the updated Meeting
+  update: protectedProcedure
+    .input(meetingsUpdateSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [updatedMeeting] = await db
+        .update(meetings)
+        .set(input)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        )
+        .returning();
 
-        if (!updatedMeeting) {
-          throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found" });
-        }
+      if (!updatedMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
 
-        return updatedMeeting;
-      }),
+      return updatedMeeting;
+    }),
 
   create: protectedProcedure
     .input(meetingsInsertSchema)
@@ -88,22 +89,36 @@ export const meetingsRouter = createTRPCRouter({
           .max(MAX_PAGE_SIZE)
           .default(DEFAULT_PAGE_SIZE),
         search: z.string().nullish(),
+        agentId: z.string().nullish(),
+        status: z
+          .enum([
+            MeetingStatus.Upcoming,
+            MeetingStatus.Active,
+            MeetingStatus.Completed,
+            MeetingStatus.Processing,
+            MeetingStatus.Cancelled,
+          ])
+          .nullish(),
       })
     )
     .query(async ({ ctx, input }) => {
-      const { search, page, pageSize } = input;
+      const { search, page, pageSize, status, agentId } = input;
       const data = await db
         .select({
           ...getTableColumns(meetings),
           agent: agents,
-          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as(
+            "duration"
+          ),
         })
         .from(meetings)
         .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined
           )
         )
         .orderBy(desc(meetings.createdAt), desc(meetings.id))
@@ -117,7 +132,9 @@ export const meetingsRouter = createTRPCRouter({
         .where(
           and(
             eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
+            search ? ilike(meetings.name, `%${search}%`) : undefined,
+            status ? eq(meetings.status, status) : undefined,
+            agentId ? eq(meetings.agentId, agentId) : undefined
           )
         );
 
